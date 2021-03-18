49e241317fb71285b41f2c5b290d94822aba81c2
Rayven <rayven.burns@gmail.com>
2021-03-18 19:18:44 -0400

    detects when bullet's rect value has a value of 0 so its deleted

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion: 
     """Overall class to manage game assests"""
@@ -33,6 +34,11 @@ class AlienInvasion:
             self.ship.update()
             # Calls bullet.update() for each bullet we place in the group bullets
             self.bullets.update()
+            # Get rid of bullets that have disappeared
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
             self._update_screen()
 
     def _check_events(self):
@@ -55,6 +61,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Respond to key releases"""
@@ -63,6 +71,10 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Update images on screen, and flip to the new screen"""
@@ -71,14 +83,16 @@ class AlienInvasion:
         self.screen.fill(self.settings.bg_color)
         #Draws the ship on the screen
         self.ship.blitme()
-
+        # Make sure each bullet is drawn to the screen before we call flip()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         #Make the most recently drawn screen visible
         pygame.display.flip()
 
 
 
 if __name__ == '__main__':
-    #Make a game instance, and run the game.
+    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
 
