65c320dc9fa0690ef8d17866de1fd86bf811cc35
Rayven <rayven.burns@gmail.com>
2021-03-18 19:44:50 -0400

    created update bullets method and moved the corresponding code into it and updated the run game method

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,13 +32,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            # Calls bullet.update() for each bullet we place in the group bullets
-            self.bullets.update()
-            # Get rid of bullets that have disappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            # print(len(self.bullets))
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -76,6 +70,17 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions
+        # Calls bullet.update() for each bullet we place in the group bullets
+        self.bullets.update()
+        # Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))
 
     def _update_screen(self):
         """Update images on screen, and flip to the new screen"""
