2a00ce60c00c7b42ae7806a38e5573feb686403a
Rayven <rayven.burns@gmail.com>
2021-03-21 00:02:44 -0400

    updated settings for fleet direction

--- settings.py
+++ settings.py
@@ -20,5 +20,8 @@ class Settings:
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
-        #Alien settings
+        # Alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet direction of represents right; -1 represents left
+        self.fleet_direction = 1
