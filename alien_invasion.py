e7c7eec9d851fc15a898019212b14c0ef632dca7
Rayven <rayven.burns@gmail.com>
2021-03-29 14:56:13 -0400

    refactored code and created new method for checking collisions between aliens and bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,9 +85,15 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        # print(len(self.bullets))
+        
+        self._check_bullet_alien_collisions()
+        
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions"""
         # Check for any bullets that have hit aliens. If so, get rid of the bullet and the alien
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
         if not self.aliens:
             # Destroy exisiting bullets and create new fleet
             self.bullets.empty() #removes bullets if self.aleins is empty
