732cdcac2b8eaf4895dc29c1d61bbeec32b4c971
Rayven <rayven.burns@gmail.com>
2021-04-02 13:14:56 -0400

    updated game stats so the game starts in an inactive state

--- game_stats.py
+++ game_stats.py
@@ -11,6 +11,9 @@ class GameStats:
         # Start Alien Invasion in an active state
         self.game_active = True
 
+        # Start game in an inactive state
+        self.game_active = False
+
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
