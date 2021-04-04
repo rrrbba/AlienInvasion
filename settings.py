658e2852d9e5e3c777e5f5266a394767d1b69e56
Rayven <rayven.burns@gmail.com>
2021-04-03 20:04:39 -0400

    changed fleet drop speed because it's too fast in windows

--- settings.py
+++ settings.py
@@ -23,6 +23,6 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 5
         # fleet direction of represents right; -1 represents left
         self.fleet_direction = 1
