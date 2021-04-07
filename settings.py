c7f729f86dc3b696c209aa188eced98250116703
Rayven <rayven.burns@gmail.com>
2021-04-06 22:26:35 -0400

    changed points amount

--- settings.py
+++ settings.py
@@ -42,7 +42,7 @@ class Settings:
         self.fleet_direction = 1
 
         # Scoring
-        self.alien_points = 50
+        self.alien_points = 15
 
     def increase_speed(self):
         """Increase speed settings"""
