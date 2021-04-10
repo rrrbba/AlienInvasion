00c308ea6643e97ee652a3ea22d8eadb9b8c44d1
Rayven <rayven.burns@gmail.com>
2021-04-10 18:25:11 -0400

    added method to check if there's a new high score

--- scoreboard.py
+++ scoreboard.py
@@ -43,6 +43,13 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
+    def check_high_scores(self):
+        """Check if there's a new high score"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
+
+
     def show_score(self):
         """Draw score to the screen"""
         self.screen.blit(self.score_image, self.score_rect)
