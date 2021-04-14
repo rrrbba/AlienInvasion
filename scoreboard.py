6e55b6dc253ec294da0b35ee22cbb5cd26c48049
Rayven <rayven.burns@gmail.com>
2021-04-14 00:00:57 -0400

    started prep level method, have to update positioning

--- scoreboard.py
+++ scoreboard.py
@@ -33,6 +33,12 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
     
+    def prep_level(self):
+        """Turn the level into a rendered image"""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+        
+
     def prep_high_score(self):
         """Turn the high score into a rendered image"""
         high_score = round(self.stats.high_score, -1)
