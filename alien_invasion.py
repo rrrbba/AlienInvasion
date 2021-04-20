8c3a01f290dc529f7d16c37b1e752d0ec15e7770
Rayven <rayven.burns@gmail.com>
2021-04-19 22:22:35 -0400

    number of ships showing on screen now

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,6 +79,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -104,6 +105,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -179,8 +181,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien"""
         if self.stats.ships_left > 0:
-            # Decrement ships_left
+            # Decrement ships_left and update scoerboard
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
--- scoreboard.py
+++ scoreboard.py
@@ -73,7 +73,8 @@ class Scoreboard:
             self.ships.add(ship)
 
     def show_score(self):
-        """Draw scores and level to the screen"""
+        """Draw scores, ships and level to the screen"""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
