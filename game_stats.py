9f8eab4485aa0c1b6479126dbd8b20a1cff7890a
Rayven <rayven.burns@gmail.com>
2021-04-19 22:52:21 -0400

    high score is saving and current high score is posting to the screen upon starting a new game

--- game_stats.py
+++ game_stats.py
@@ -15,7 +15,8 @@ class GameStats:
         self.game_active = False
 
         # High score should never be reset
-        self.high_score = 0
+        with open('highscore.txt', 'r') as reader:
+            self.high_score = reader.read()
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
--- scoreboard.py
+++ scoreboard.py
@@ -48,7 +48,7 @@ class Scoreboard:
 
     def prep_high_score(self):
         """Turn the high score into a rendered image"""
-        high_score = round(self.stats.high_score, -1)
+        high_score = round(int(self.stats.high_score), -1)
         high_score_str = "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
 
@@ -59,7 +59,7 @@ class Scoreboard:
 
     def check_high_score(self):
         """Check if there's a new high score"""
-        if self.stats.score > self.stats.high_score:
+        if int(self.stats.score) > int(self.stats.high_score):
             self.stats.high_score = self.stats.score
             self.prep_high_score()
             with open('highscore.txt', 'w') as writer:
